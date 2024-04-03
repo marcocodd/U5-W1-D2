44531d1d2ec8ce2dce2d862a0eea5e0b3514a457
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-03 16:49:17 +0200

    aggiornato My runner

--- src/main/java/com/epicode/U5D1/MyRunner.java
+++ src/main/java/com/epicode/U5D1/MyRunner.java
@@ -6,24 +6,29 @@ import com.epicode.U5D1.entities.StateOrder;
 import com.epicode.U5D1.entities.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyRunner implements CommandLineRunner {
+
     @Autowired
     private Menu menu;
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println("CIAO PROVA");
         Table table1 = new Table(1, 4, true);
         Order order1 = new Order(1, StateOrder.Servito, table1.getMaxPersons(), table1);
         order1.addToOrder(menu.getPizzaList().get(1));
         order1.addToOrder(menu.getPizzaList().get(2));
         order1.addToOrder(menu.getPizzaList().get(0));
 
-        order1.totalOrderPrice(); // Calcola il prezzo totale dell'ordine
+        order1.totalOrderPrice();
 
-        // Stampa dell'ordine
+
         System.out.println("Ordine:");
-        System.out.println(order1);
+        System.out.println("numero ordine" + order1.getOrderNumber());
+        System.out.println(order1.getFood());
+        System.out.println(order1.getTotalPrice());
     }
 }
